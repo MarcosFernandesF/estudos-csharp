97b8f19ec90d32497b24e6b16465e780a954c7eb
marcosrff15@gmail.com <marcosrff15@gmail.com>
2022-11-01 23:26:55 -0300

    Aula 02: Nosso primeiro programa e o Visual Studio
    - Instalando Visual Studio
    
    - Fazendo o primeiro programa

---
+++ AprendendoCSharp/explorando-csharp/1-OláMundo/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, Mundo");
+        Console.WriteLine("Tecle enter para sair...");
+        Console.ReadLine();
+    }
+}
