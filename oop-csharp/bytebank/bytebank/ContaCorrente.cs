9b6011c18c1b3f28a8c8fc5336bbb385b4e0cb46
marcosrff15@gmail.com <marcosrff15@gmail.com>
2022-11-09 16:21:11 -0300

    Aula 02: Entendendo tipos
    - Os tipos por referência só apontam para um objeto criado na memória do computador
    
    - Como manipular os valores dos campos de um objeto

--- oop-csharp/bytebank/bytebank/ContaCorrente.cs
+++ oop-csharp/bytebank/bytebank/ContaCorrente.cs
@@ -12,5 +12,40 @@ namespace bytebank
         public string conta;
         public int numeroAgencia;
         public double saldo;
+
+        // Método público e que não retorna nada (Void)
+        public void Depositar(double valor)
+        {
+            // This representa o próprio objeto, é como se fosse um self do python
+            this.saldo += valor;
+        }
+
+        // Método público que retorna um valor booleano (True or False)
+        public bool Sacar(double valor)
+        {
+            if (valor <= saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+        }
     }
 }
