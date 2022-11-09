7853806f3272471af3cafa128b8b5421a5726dc0
marcosrff15@gmail.com <marcosrff15@gmail.com>
2022-11-09 15:24:00 -0300

    Aula 01: Criando a primeira classe
    - Sintaxe básica de uma classe em C#
    
    - Criar objetos a partir da definição de uma classe usando new

---
+++ oop-csharp/bytebank/bytebank/ContaCorrente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public string conta;
+        public int numeroAgencia;
+        public double saldo;
+    }
+}
---
+++ oop-csharp/bytebank/bytebank/Program.cs
@@ -0,0 +1,11 @@
+﻿using bytebank;
+
+// Instanciando um objeto ContaCorrente em uma variável do tipo ContaCorrente
+ContaCorrente contaDoAndre = new ContaCorrente();
+// É possível acessar os atributos da classe pois são definidos como Publicos
+contaDoAndre.titular = "André Silva";
+contaDoAndre.numeroAgencia = 15;
+contaDoAndre.conta = "1010-X";
+contaDoAndre.saldo = 100;
+
+Console.WriteLine("Saldo da conta do André = " + contaDoAndre.saldo);
