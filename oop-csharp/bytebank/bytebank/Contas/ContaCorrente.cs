c139d4b7f0abb0ddca089711b3a3dc28fd2851b8
marcosrff15@gmail.com <marcosrff15@gmail.com>
2022-11-10 16:12:01 -0300

    Aula 04: Namespace e compor classe
    - Organizar a estrutura do projeto com os Namespaces
    
    - Conceito de composição
    
    - Referencia nula de objeto

--- oop-csharp/bytebank/bytebank/Contas/ContaCorrente.cs
+++ oop-csharp/bytebank/bytebank/Contas/ContaCorrente.cs
@@ -1,23 +1,25 @@
-﻿using System;
+﻿using bytebank.Titular;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace bytebank
+namespace bytebank.Contas
 {
     public class ContaCorrente
     {
-        public string titular; // String Valor padrão = Vazio
         public string conta; // String Valor padrão = Vazio
         public int numeroAgencia; // Int Valor padrão = 0
         public double saldo = 100; // Double Valor padrão = 0
+
+        public Cliente titular; // Composição: titular vai receber uma classe instanciada fora de ContaCorrente
 
         // Método público e que não retorna nada (Void)
         public void Depositar(double valor)
         {
             // This representa o próprio objeto, é como se fosse um self do python
-            this.saldo += valor;
+            saldo += valor;
         }
 
         // Método público que retorna um valor booleano (True or False)
@@ -25,7 +27,7 @@ namespace bytebank
         {
             if (valor <= saldo)
             {
-                this.saldo -= valor;
+                saldo -= valor;
                 return true;
             }
             else
@@ -33,16 +35,16 @@ namespace bytebank
                 return false;
             }
         }
-        
+
         public bool Transferir(double valor, ContaCorrente destino)
         {
-            if (this.saldo < valor)
+            if (saldo < valor)
             {
                 return false;
             }
             else
             {
-                this.Sacar(valor);
+                Sacar(valor);
                 destino.Depositar(valor);
                 return true;
             }
--- oop-csharp/bytebank/bytebank/Program.cs
+++ oop-csharp/bytebank/bytebank/Program.cs
@@ -1,22 +1,23 @@
-﻿using bytebank;
+﻿using bytebank.Contas;
+using bytebank.Titular;
 
-// Instanciando um objeto ContaCorrente em uma variável do tipo ContaCorrente
-ContaCorrente contaDoAndre = new ContaCorrente();
-// É possível acessar os atributos da classe pois são definidos como Publicos
-contaDoAndre.titular = "André Silva";
-contaDoAndre.numeroAgencia = 15;
-contaDoAndre.conta = "1010-X";
-contaDoAndre.saldo = 100;
+//// Instanciando um objeto ContaCorrente em uma variável do tipo ContaCorrente
+//ContaCorrente contaDoAndre = new ContaCorrente();
+//// É possível acessar os atributos da classe pois são definidos como Publicos
+//contaDoAndre.titular = "André Silva";
+//contaDoAndre.numeroAgencia = 15;
+//contaDoAndre.conta = "1010-X";
+//contaDoAndre.saldo = 100;
 
-ContaCorrente contaDoAndre2 = new ContaCorrente();
-contaDoAndre2.titular = "André Silva";
-contaDoAndre2.numeroAgencia = 15;
-contaDoAndre2.conta = "1010-X";
-contaDoAndre2.saldo = 100;
+//ContaCorrente contaDoAndre2 = new ContaCorrente();
+//contaDoAndre2.titular = "André Silva";
+//contaDoAndre2.numeroAgencia = 15;
+//contaDoAndre2.conta = "1010-X";
+//contaDoAndre2.saldo = 100;
 
-contaDoAndre = contaDoAndre2; // Igualando os endereços
+//contaDoAndre = contaDoAndre2; // contaDoAndre agora aponta para o endereço do contaDoAndre2
 
-Console.WriteLine(contaDoAndre == contaDoAndre2); //Tipagem por referência, compara endereços de memória
+//Console.WriteLine(contaDoAndre == contaDoAndre2); //Tipagem por referência, compara endereços de memória
 
 //Console.WriteLine("Saldo da conta do André = " + contaDoAndre.saldo);
 
@@ -47,5 +48,32 @@ Console.WriteLine(contaDoAndre == contaDoAndre2); //Tipagem por referência, com
 //Console.WriteLine(valor);
 //Console.WriteLine(valor2);
 
+Cliente cliente = new Cliente();
+cliente.nome = "André Silva";
+cliente.cpf = "123456789";
+cliente.profissao = "Analista";
 
+ContaCorrente conta = new ContaCorrente();
+conta.titular = cliente;
+conta.conta = "1010-X";
+conta.numeroAgencia = 15;
+conta.saldo = 100;
+
+Console.WriteLine("Titular = " + conta.titular.nome);
+Console.WriteLine("CPF = " + conta.titular.cpf);
+Console.WriteLine("Profissão = " + conta.titular.profissao);
+Console.WriteLine("Nº Conta = " + conta.conta);
+Console.WriteLine("Saldo= " + conta.saldo);
+Console.WriteLine("Nº Agência = " + conta.numeroAgencia);
+
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = new Cliente();
+conta2.titular.nome = "José Souza";
+conta2.titular.cpf = "98765321";
+conta2.titular.profissao = "Tester";
+conta2.conta = "9999-X";
+conta2.numeroAgencia = 18;
+conta2.saldo = 500;
+
+Console.WriteLine(conta2.titular.nome);
 
